30ee374b034f6d14d3a61c04a0b2534286d9d5a1
drekuu <drekuuu@gmail.com>
2025-04-20 02:19:23 +0200

    fix: blobtobase64 fix

--- src/utils/blob.ts
+++ src/utils/blob.ts
@@ -1,12 +1,4 @@
-export function blobToBase64(blob: Blob): Promise<string> {
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.onloadend = () => {
-      const dataUrl = reader.result as string;
-      const base64 = dataUrl.split(',')[1]; // remove 'data:application/pdf;base64,'
-      resolve(base64);
-    };
-    reader.onerror = reject;
-    reader.readAsDataURL(blob);
-  });
+export async function blobToBase64(blob: Blob): Promise<string> {
+  const arrayBuffer = await blob.arrayBuffer();
+  return Buffer.from(arrayBuffer).toString('base64');
 }

58cde0b62bdd26ac6eca7545aee368c2d7902fc8
drekuu <drekuuu@gmail.com>
2025-08-26 02:19:37 +0200

    Revert "feat: add fullscreen button"
    This reverts commit 5577def124994dd38d773da9698b0b0e7837ee46.

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,6 @@ import ClientBalances from './pages/ClientBalance/ClientBalances.tsx';
 import Return from './pages/Return.tsx';
 import './App.css';
 import ClientBalance from './pages/ClientBalance/ClientBalance.tsx';
-import { Fullscreen } from 'lucide-react';
-import { useCallback, useState } from 'react';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -26,34 +24,6 @@ const queryClient = new QueryClient({
 });
 
 function App() {
-  const [isFullscreen, setIsFullscreen] = useState(false);
-
-  const handleToggleFullscreen = useCallback(() => {
-    if (!isFullscreen) {
-      const elem = document.documentElement;
-
-      if (elem.requestFullscreen) {
-        void elem.requestFullscreen();
-      } else if ((elem as any).webkitRequestFullscreen) {
-        (elem as any).webkitRequestFullscreen();
-      } else if ((elem as any).msRequestFullscreen) {
-        (elem as any).msRequestFullscreen();
-      }
-
-      setIsFullscreen(true);
-    } else {
-      if (document.exitFullscreen) {
-        void document.exitFullscreen();
-      } else if ((document as any).webkitExitFullscreen) {
-        (document as any).webkitExitFullscreen();
-      } else if ((document as any).msExitFullscreen) {
-        (document as any).msExitFullscreen();
-      }
-
-      setIsFullscreen(false);
-    }
-  }, [isFullscreen]);
-
   return (
     <QueryClientProvider client={queryClient}>
       <ToastContainer />
@@ -62,13 +32,6 @@ function App() {
         <Router>
           <div className='app'>
             <header className='app-header'>
-              <Fullscreen
-                onClick={() => handleToggleFullscreen()}
-                width={50}
-                height={50}
-                className='cursor-pointer absolute top-2 left-2'
-              />
-
               <Routes>
                 <Route path='/split-order' element={<SplitOrder />} />
                 <Route path='/order' element={<Order />} />

f1eeadc31bf7ca41171b48c79cb162af643275cd
drekuu <drekuuu@gmail.com>
2025-09-13 15:31:17 +0200

    fix: use brutto prices everywhere

--- src/api/comarch/document.ts
+++ src/api/comarch/document.ts
@@ -7,8 +7,6 @@ import { getOrderDocuments, updateOrderDocument } from '../bitrix/order.ts';
 import { getDeal, updateDealReturnData } from '../bitrix/deal.ts';
 import moment from 'moment/moment';
 import { getCompanyCode, getContactCode } from '@/utils/bitrix24.ts';
-import { convertItemPrice } from '@/utils/item.ts';
-import { PriceType } from '@/data/comarch/prices.ts';
 
 export type AddDocument = {
   placementId: number;
@@ -109,18 +107,13 @@ export function useAddDocument(token: string) {
 
       const body: any = {
         type: documentType,
-        calculatedOn: 1,
+        calculatedOn: 2,
         paymentMethod: 'przelew',
         currency: 'PLN',
         elements: order.items.map((item) => ({
           code: item.warehouseCode,
           quantity: item.quantity,
-          totalNetValue: convertItemPrice(
-            item.unitPrice * item.quantity,
-            item.taxRate ?? 23,
-            PriceType.BRUTTO,
-            PriceType.NETTO,
-          ),
+          totalGrossValue: item.unitPrice * item.quantity,
           setCustomValue: true,
         })),
         payer: buyerNoAddress,

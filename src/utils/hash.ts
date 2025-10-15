6e6018fea7dbc2bcffaf15a1a2611beab93719d5
drekuu <drekuuu@gmail.com>
2025-10-15 12:27:32 +0200

    fix: clamp measure hash to nonnegative int32

--- src/utils/hash.ts
+++ src/utils/hash.ts
@@ -2,10 +2,9 @@ export async function getHashCode(str: string): Promise<string> {
   const textAsBuffer = new TextEncoder().encode(str);
   const hashBuffer = await window.crypto.subtle.digest('SHA-256', textAsBuffer);
 
-  const hashArray = Array.from(new Uint8Array(hashBuffer));
-  const hash = hashArray
-    .map((item) => item.toString(16).padStart(2, '0'))
-    .join('');
+  const view = new DataView(hashBuffer);
+  const int32 = view.getInt32(0, false);
 
-  return parseInt(hash.slice(0, 8), 16).toString();
+  const clamped = int32 & 0x7fffffff; // remove negative sign
+  return clamped.toString();
 }
